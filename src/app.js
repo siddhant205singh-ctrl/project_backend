2ce09a8eae3f8086379a7908d9c88e73722ca68a
Siddhantt Singh <siddhant205singh@gmail.com>
2026-06-23 13:18:13 +0530

    utils code added

--- src/app.js
+++ src/app.js
@@ -6,13 +6,13 @@ import cookieParser from "cookierParser"
 
 const app = express()
 app.use(cors({
-    origin:process.env.CORS_ORIGIN,
+    origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
 app.use(express.json({
     limit:"16kb"
 }))
 app.use(express.urlencoded({extended: true,limit: "16kb"}))
-
+app.use(express.static("public"))
 app.use(cookieParser())
 export{ app}
