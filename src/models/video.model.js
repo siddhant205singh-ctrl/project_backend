cf334e93476d2380f74877cef56079e2178fa3c1
Siddhantt Singh <siddhant205singh@gmail.com>
2026-06-29 13:01:51 +0530

    made user model

---
+++ src/models/video.model.js
@@ -0,0 +1,43 @@
+import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+const videoSchema = new Schema({
+videoFile:{
+    type:String ,// cloudinary part
+    required : true
+},
+thumbnail:{
+    type:String ,// cloudinary part
+    required : true
+},
+title:{
+    type:String ,
+    required : true
+},
+description:{
+    type:String ,
+    required : true
+},
+duration:{
+    type:Number,// cloudinary part
+    required : true
+},
+view:{
+    type:Number,
+    default:0
+},
+isPublished:{
+    type:Boolean,
+    default : true
+},
+owner:{
+    type:Schema.Types.ObjectId,
+    ref = "User"
+}
+
+
+
+
+},{timestamps:true})
+
+videoSchema.plugin(mongooseAggregatePaginate)
+export const Video = mongoose.model("Video",videoschema)
