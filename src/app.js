90e5b889f87116b6ff4c92f74b33240a1ce80356
Siddhantt Singh <siddhant205singh@gmail.com>
2026-06-30 22:26:17 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 import express from "express"
 import cors from "cors"
-import cookieParser from "cookierParser"
+import cookieParser from "cookie-parser"
 
 
 
-const app = express()
+const app = express();
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
@@ -15,4 +15,11 @@ app.use(express.json({
 app.use(express.urlencoded({extended: true,limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+// routes import
+
+import userRouter from './routes/user.routes.js'
+// route declaration
+app.use("/api/v1/users", userRouter)
+//http://localhost:8000/api/v1/users/register
 export{ app}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 //require("dotenv").config({path: './env'})
 import dotenv from "dotenv"
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants.js";
+//import mongoose from "mongoose";
+//import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
+//import express from "express"
+//const app = express();
+import {app} from "./app.js"
 dotenv.config({
      path: './.env'
  })
